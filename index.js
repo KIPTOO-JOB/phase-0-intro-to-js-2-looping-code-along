9a98722beb2d0b46b989a25776fd23c4b030ca80
JOB KIPTOO <114221274+KIPTOO-JOB@users.noreply.github.com>
2024-07-19 10:35:21 +0000

    loop

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+
+countDown(10);
+
+const  writeCards = (names, event)=>{
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+let namesArray = ["Guadalupe", "Ollie", "Aki"];
+let event = "surprise";
+let thankYouMessages = writeCards(namesArray, event);
+console.log(thankYouMessages);
